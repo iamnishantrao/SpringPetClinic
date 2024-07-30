847bbff2ea84f64dcecefb25ed033f14d79da232
Nishant Yadav <nishantyadav025@gmail.com>
2024-07-29 17:55:58 -0700

    Create Base Crud Service

--- data/src/main/java/org/example/data/services/PetService.java
+++ data/src/main/java/org/example/data/services/PetService.java
@@ -2,13 +2,5 @@ package org.example.data.services;
 
 import org.example.data.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
